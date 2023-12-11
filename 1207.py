c63e5914a070f69092e172f449d52a9a60c78ff2
walter.ns <gmlsto@gmail.com>
2023-12-11 13:34:32 +0900

    refactoring

--- 1207.py
+++ 1207.py
@@ -1,7 +1,6 @@
 import openai
 import json
 from google.colab import drive
-import jsonlines
 
 def complete_message(messages, temperature=1, max_tokens=4293):
 
@@ -41,34 +40,6 @@ def read_contents(file_path) :
       print(f"An error occurred: {e}")
 
 
-def attach_file(file_path) :
-  # response = openai.File.create(file=open(file_path, "rb"), purpose='answers')
-  response = openai.File.create(file=open(file_path, "rb"), purpose='assistants')
-  # response = openai.File.create(file=open(file_path, "rb"), purpose='fine-tune')
-  file_id = response['id']
-  return file_id
-
-
-def complete_prompt_with_file(prompt, file_path, temperature=1, max_tokens=4293):
-  file_id = attach_file(file_path)
-  # 파일 ID를 사용하여 GPT 모델에 요청 보내기
-  response = openai.Completion.create(
-    model="text-davinci-003",
-    #model="gpt-3.5-turbo",
-    prompt=prompt,
-    file=file_id  # 파일 ID 사용
-    )
-  return completion.choices[0].message.content
-
-
-
-def write_json_lines(data):
-    with open('upload-files.JSONL', 'wb') as f:
-        writer = jsonlines.Writer(f)
-        for i in data:
-            line = {"role": "user","content": i}
-            writer.write(line)
-
 
 drive.mount('/content/drive')
 
@@ -82,7 +53,7 @@ prompt = """
 입력 데이터에서 '#'로 시작하는 문장은 제목에 해당해.
 '-'로 시작하고 ':'로 끝나는 문장은 제목이야.
 문장에 '|'가 두 개 이상있다면 문장의 처음부터 첫번쩨 '|'까지가 제목이야 
-문장에 ':'가 하나 이상 있다면 처음부터 첫번쩨 '|'까지가 제목이야
+문장에 ':'가 하나 이상 있다면 처음부터 첫번쩨 ':'까지가 제목이야
 """
 result = complete_prompt_with_data(prompt, contents, 1, 8500)
 print(result)
