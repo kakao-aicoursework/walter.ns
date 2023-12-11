9fd928c5930ec75e39dde049f4aae5e228bd4f1d
walter.ns <gmlsto@gmail.com>
2023-12-11 13:29:23 +0900

    bugfix

--- 1207.py
+++ 1207.py
@@ -6,7 +6,7 @@ import jsonlines
 def complete_message(messages, temperature=1, max_tokens=4293):
 
   completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-3.5-turbo-16k",
         messages=messages,
         temperature=temperature,
         max_tokens=max_tokens,
@@ -74,8 +74,15 @@ drive.mount('/content/drive')
 
 file_path = "/content/drive/MyDrive/datas/kt_c.txt"
 
+file_path = "/content/drive/MyDrive/datas/kt_c.txt"
+contents = read_contents(file_path)
+
 prompt = """
-첨부된 파일을 기반으로 데이터를 제목과 내용으로 정형화해줘.
+다음 데이터를 '제목 : 내용'으로 형식으로 정형화해줘.
+입력 데이터에서 '#'로 시작하는 문장은 제목에 해당해.
+'-'로 시작하고 ':'로 끝나는 문장은 제목이야.
+문장에 '|'가 두 개 이상있다면 문장의 처음부터 첫번쩨 '|'까지가 제목이야 
+문장에 ':'가 하나 이상 있다면 처음부터 첫번쩨 '|'까지가 제목이야
 """
-result = complete_prompt_with_file(prompt, file_path)
+result = complete_prompt_with_data(prompt, contents, 1, 8500)
 print(result)
